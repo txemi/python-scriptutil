513439f3be36ebfcf025116373fdb9ec09fab6d0
Txemi Martinez <josemiguel.martinez@isetic.com>
2017-04-04 17:21:23 +0200

    first version of setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+ld = """This package contains a python module which provides a recursive find
+ on the filesystem and searching within those files."""
+setup(name='scriptutil', version='1.0', description='Python module which provides the functionality of find and grep',
+      long_description=ld, url='http://hrnjad.net/src/7/scriptutil.py', packages=find_packages())
